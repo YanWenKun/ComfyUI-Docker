0d27a97a16d3c6ae36f6fc9f3b6042b80f49bf7d
Pete Myron <pete.myron@gmail.com>
2023-04-20 11:57:50 -0500

    resume aria2 downloading if interrupted on first attempt

--- scripts/download.sh
+++ scripts/download.sh
@@ -7,11 +7,11 @@ echo "########################################"
 set -euxo pipefail
 
 cd /home/runner
-git clone https://github.com/comfyanonymous/ComfyUI.git
+git clone https://github.com/comfyanonymous/ComfyUI.git || (cd /home/runner/ComfyUI && git pull)
 
 # Controlnet Preprocessor nodes by Fannovel16
 cd /home/runner/ComfyUI/custom_nodes
-git clone https://github.com/Fannovel16/comfy_controlnet_preprocessors
+git clone https://github.com/Fannovel16/comfy_controlnet_preprocessors || (cd /home/runner/ComfyUI/custom_nodes/comfy_controlnet_preprocessors && git pull)
 
 cd /home/runner/ComfyUI
 aria2c --allow-overwrite=false --auto-file-renaming=false --continue=true \
