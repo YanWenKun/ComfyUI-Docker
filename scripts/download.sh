3185e28c6bd607acb3a1c6122cd9c176a0866209
YAN Wenkun <code@yanwk.fun>
2023-10-04 12:29:02 +0800

    Add ComfyUI Manager

--- scripts/download.sh
+++ scripts/download.sh
@@ -17,6 +17,12 @@ git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
     https://github.com/Fannovel16/comfyui_controlnet_aux.git \
     || (cd /home/runner/ComfyUI/custom_nodes/comfyui_controlnet_aux && git pull)
 
+# ComfyUI Manager
+cd /home/runner/ComfyUI/custom_nodes
+git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
+    https://github.com/ltdrdata/ComfyUI-Manager.git \
+    || (cd /home/runner/ComfyUI/custom_nodes/ComfyUI-Manager && git pull)
+
 cd /home/runner/ComfyUI
 aria2c --allow-overwrite=false --auto-file-renaming=false --continue=true \
     --max-connection-per-server=5 --input-file=/home/scripts/download.txt
