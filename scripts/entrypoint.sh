20bc10ad5f20991455b0e510d94b7648bd98ccc2
YAN Wenkun <code@yanwk.fun>
2023-10-17 19:04:26 +0800

    No longer auto-update at every start (We have Manager now)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,16 +4,10 @@ set -e
 
 cd /home/runner
 
-# Install ComfyUI or update it.
+# Install ComfyUI.
 if [ ! -f "/home/runner/.download-complete" ] ; then
     chmod +x /home/scripts/download.sh
     bash /home/scripts/download.sh
-else
-    # If update failed, just skip.
-    set +e
-    chmod +x /home/scripts/update.sh
-    bash /home/scripts/update.sh
-    set -e
 fi ;
 
 echo "########################################"
