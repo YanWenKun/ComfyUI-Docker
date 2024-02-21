7dadff13db0da6be206d7bb2479652acc3ca724d
YAN Wenkun <code@yanwk.fun>
2024-02-21 08:42:47 +0800

    Update downloads

--- scripts/download.sh
+++ scripts/download.sh
@@ -11,12 +11,6 @@ git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
     https://github.com/comfyanonymous/ComfyUI.git \
     || (cd /home/runner/ComfyUI && git pull)
 
-# ControlNet Auxiliary Preprocessors by Fannovel16
-cd /home/runner/ComfyUI/custom_nodes
-git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
-    https://github.com/Fannovel16/comfyui_controlnet_aux.git \
-    || (cd /home/runner/ComfyUI/custom_nodes/comfyui_controlnet_aux && git pull)
-
 # ComfyUI Manager
 cd /home/runner/ComfyUI/custom_nodes
 git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
@@ -24,7 +18,8 @@ git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
     || (cd /home/runner/ComfyUI/custom_nodes/ComfyUI-Manager && git pull)
 
 cd /home/runner/ComfyUI
-aria2c --allow-overwrite=false --auto-file-renaming=false --continue=true \
-    --max-connection-per-server=5 --input-file=/home/scripts/download.txt
+aria2c --input-file=/home/scripts/download.txt \
+    --allow-overwrite=false --auto-file-renaming=false --continue=true \
+    --max-connection-per-server=5
 
 touch /home/runner/.download-complete
