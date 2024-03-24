7dd70bf37b9097caf374c530b3d03fc92e9985fe
YAN Wenkun <code@yanwk.fun>
2024-03-24 11:28:13 +0800

    Add set-proxy to be excuted first in entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+# Run user's set-proxy script
+cd /home/runner
+if [ -f "/home/runner/scripts/set-proxy.sh" ] ; then
+    echo "[INFO] Running set-proxy script..."
+
+    chmod +x /home/runner/scripts/set-proxy.sh
+    source /home/runner/scripts/set-proxy.sh
+fi ;
+
+# Install ComfyUI
+cd /home/runner
+if [ ! -f "/home/runner/.download-complete" ] ; then
+    chmod +x /home/scripts/download.sh
+    bash /home/scripts/download.sh
+fi ;
+
 # Run user's pre-start script
 cd /home/runner
 if [ -f "/home/runner/scripts/pre-start.sh" ] ; then
@@ -11,13 +27,6 @@ if [ -f "/home/runner/scripts/pre-start.sh" ] ; then
     source /home/runner/scripts/pre-start.sh
 else
     echo "[INFO] No pre-start script found. Skipping."
-fi ;
-
-# Install ComfyUI
-cd /home/runner
-if [ ! -f "/home/runner/.download-complete" ] ; then
-    chmod +x /home/scripts/download.sh
-    bash /home/scripts/download.sh
 fi ;
 
 
