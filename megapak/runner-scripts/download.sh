51f9a90c65e8f763a0bea25c5b608dd8bee3691c
YAN Wenkun <code@yanwk.fun>
2024-04-13 08:51:44 +0800

    Tune downloads [skip ci]

--- megapak/runner-scripts/download.sh
+++ megapak/runner-scripts/download.sh
@@ -89,9 +89,9 @@ echo "[INFO] Downloading Models..."
 echo "########################################"
 
 # Models
-cd /root/ComfyUI
+cd /root/ComfyUI/models
 aria2c \
-  --input-file=/runner-scripts/download.txt \
+  --input-file=/runner-scripts/download-models.txt \
   --allow-overwrite=false \
   --auto-file-renaming=false \
   --continue=true \
