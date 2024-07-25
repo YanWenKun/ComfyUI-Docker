c375863421ddfe33c98afd429728c9ef88af3cc1
YAN Wenkun <code@yanwk.fun>
2024-07-26 02:48:10 +0800

    Add 'comfy3d-pt22'

---
+++ comfy3d-pt22/runner-scripts/download.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Regex that matches REPO_NAME
+# First from pattern [https://example.com/xyz/REPO_NAME.git] or [git@example.com:xyz/REPO_NAME.git]
+# Second from pattern [http(s)://example.com/xyz/REPO_NAME]
+# They all extract REPO_NAME to BASH_REMATCH[2]
+function clone_or_pull () {
+    if [[ $1 =~ ^(.*[/:])(.*)(\.git)$ ]] || [[ $1 =~ ^(http.*\/)(.*)$ ]]; then
+        echo "${BASH_REMATCH[2]}" ;
+        set +e ;
+            git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules "$1" \
+                || git -C "${BASH_REMATCH[2]}" pull --ff-only ;
+        set -e ;
+    else
+        echo "[ERROR] Invalid URL: $1" ;
+        return 1 ;
+    fi ;
+}
+
+
+echo "########################################"
+echo "[INFO] Downloading ComfyUI & Manager..."
+echo "########################################"
+
+cd /root
+clone_or_pull https://github.com/comfyanonymous/ComfyUI.git
+
+cd /root/ComfyUI/custom_nodes
+clone_or_pull https://github.com/ltdrdata/ComfyUI-Manager.git
+
+
+echo "########################################"
+echo "[INFO] Downloading Custom Nodes..."
+echo "########################################"
+
+cd /root/ComfyUI/custom_nodes
+
+clone_or_pull https://github.com/MrForExample/ComfyUI-3D-Pack.git
+git -C ComfyUI-3D-Pack reset --hard 3b4e715939376634c68aa4c1c7d4ea4a8665c098
+
+
+# Finish
+touch /root/.download-complete
