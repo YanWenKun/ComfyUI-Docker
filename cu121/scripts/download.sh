bfef4db1569e8fba3f207199fb7189cf3eec94e4
YAN Wenkun <code@yanwk.fun>
2024-09-06 05:36:59 +0800

    'cu121' Using stable version of ComfyUI

--- cu121/scripts/download.sh
+++ cu121/scripts/download.sh
@@ -7,10 +7,17 @@ echo "########################################"
 set -euxo pipefail
 
 # ComfyUI
+# Using stable version (has a release tag)
 cd /home/runner
-git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
-    https://github.com/comfyanonymous/ComfyUI.git \
-    || (cd /home/runner/ComfyUI && git pull)
+(
+    git clone https://github.com/comfyanonymous/ComfyUI.git &&
+    cd ComfyUI &&
+    git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
+) ||
+# If fails, try git-pull
+(
+    cd /home/runner/ComfyUI && git pull
+)
 
 # ComfyUI Manager
 cd /home/runner/ComfyUI/custom_nodes
