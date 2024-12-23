3076d7203e3e99f5f23f99293cc67f02d6e347c6
YAN Wenkun <code@yanwk.fun>
2024-12-23 13:03:59 +0800

    'rocm' Major upgrade to 6.3.1, Python 3.12

--- rocm/runner-scripts/download.sh
+++ rocm/runner-scripts/download.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-# Regex that matches REPO_NAME
-# First from pattern [https://example.com/xyz/REPO_NAME.git] or [git@example.com:xyz/REPO_NAME.git]
-# Second from pattern [http(s)://example.com/xyz/REPO_NAME]
-# They all extract REPO_NAME to BASH_REMATCH[2]
+# Note: the "${BASH_REMATCH[2]}" here is REPO_NAME from
+# [https://example.com/somebody/REPO_NAME.git] or [git@example.com:somebody/REPO_NAME.git]
 function clone_or_pull () {
     if [[ $1 =~ ^(.*[/:])(.*)(\.git)$ ]] || [[ $1 =~ ^(http.*\/)(.*)$ ]]; then
         echo "${BASH_REMATCH[2]}" ;
@@ -24,8 +22,13 @@ echo "########################################"
 echo "[INFO] Downloading ComfyUI & Manager..."
 echo "########################################"
 
+set +e
 cd /root
-clone_or_pull https://github.com/comfyanonymous/ComfyUI.git
+git clone https://github.com/comfyanonymous/ComfyUI.git || git -C "ComfyUI" pull --ff-only
+cd /root/ComfyUI
+# Using stable version (has a release tag)
+git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
+set -e
 
 cd /root/ComfyUI/custom_nodes
 clone_or_pull https://github.com/ltdrdata/ComfyUI-Manager.git
@@ -37,31 +40,28 @@ echo "########################################"
 
 cd /root/ComfyUI/custom_nodes
 
-# Workspace
-clone_or_pull https://github.com/AIGODLIKE/AIGODLIKE-ComfyUI-Translation.git
-clone_or_pull https://github.com/crystian/ComfyUI-Crystools.git
-clone_or_pull https://github.com/crystian/ComfyUI-Crystools-save.git
-
 # General
 clone_or_pull https://github.com/chrisgoringe/cg-use-everywhere.git
 clone_or_pull https://github.com/cubiq/ComfyUI_essentials.git
-clone_or_pull https://github.com/jags111/efficiency-nodes-comfyui.git
 clone_or_pull https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+clone_or_pull https://github.com/WASasquatch/was-node-suite-comfyui.git
 
 # Control
+clone_or_pull https://github.com/cubiq/ComfyUI_InstantID.git
 clone_or_pull https://github.com/cubiq/ComfyUI_IPAdapter_plus.git
+clone_or_pull https://github.com/cubiq/PuLID_ComfyUI.git
 clone_or_pull https://github.com/Fannovel16/comfyui_controlnet_aux.git
 clone_or_pull https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git
 clone_or_pull https://github.com/ltdrdata/ComfyUI-Impact-Pack.git
 clone_or_pull https://github.com/ltdrdata/ComfyUI-Impact-Subpack.git
 clone_or_pull https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git
-clone_or_pull https://github.com/twri/sdxl_prompt_styler.git
 
 # Video
-clone_or_pull https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git
 clone_or_pull https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git
 clone_or_pull https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
 
+# Other
+clone_or_pull https://github.com/cubiq/ComfyUI_FaceAnalysis.git
 
 echo "########################################"
 echo "[INFO] Downloading Models..."
