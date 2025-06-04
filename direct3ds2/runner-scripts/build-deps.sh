02a506b1851b64b614a3867eedd73590dea4d7f5
YAN Wenkun <code@yanwk.fun>
2025-06-05 04:28:03 +0800

    'direct3ds2' better runner flow

--- direct3ds2/runner-scripts/build-deps.sh
+++ direct3ds2/runner-scripts/build-deps.sh
@@ -13,12 +13,6 @@ if [ -z "${CMAKE_ARGS}" ]; then
     echo "[INFO] CMAKE_ARGS not set, setting to ${CMAKE_ARGS}"
 fi ;
 
-# (Optional) Compile Flash Attention for Ampere and later GPUs.
-# "MAX_JOBS" limits Ninja jobs to avoid OOM.
-# If have >96GB RAM, just remove MAX_JOBS line.
-# export MAX_JOBS=4
-# pip install flash-attn --no-build-isolation
-
 cd /root/Direct3D-S2/third_party/voxelize
 pip install .
 
@@ -27,6 +21,23 @@ pip install "git+https://github.com/mit-han-lab/torchsparse.git"
 
 cd /root/Direct3D-S2
 pip install .
+
+# (Optional) Compile Flash Attention for Ampere and later GPUs.
+# "MAX_JOBS" limits Ninja jobs to avoid OOM.
+# If have >96GB RAM, just remove MAX_JOBS line.
+cd /root
+if [ "$ATTN_BACKEND" == "flash_attn" ] || [ "$SPARSE_ATTN_BACKEND" == "flash_attn" ]; then
+    echo "########################################"
+    echo "[INFO] Compile-Installing Flash Attention..."
+    echo "########################################"
+
+    export MAX_JOBS=4
+    pip install flash-attn --no-build-isolation
+
+    echo "########################################"
+    echo "[INFO] Successfully Installed Flash Attention."
+    echo "########################################"
+fi
 
 # Finish
 touch /root/.build-complete
--- direct3ds2/runner-scripts/download-models.sh
+++ direct3ds2/runner-scripts/download-models.sh
@@ -6,8 +6,12 @@ echo "########################################"
 echo "[INFO] Downloading Models..."
 echo "########################################"
 
-# Models
 cd /root
+
+set +e
+
 huggingface-cli download "wushuang98/Direct3D-S2" --include "direct3d-s2-v-1-1/*"
 huggingface-cli download "ZhengPeng7/BiRefNet"
 python3 -c "import torch; torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14_reg');"
+
+set -e
