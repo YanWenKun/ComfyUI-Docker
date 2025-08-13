b82f03832621e80b611b5239c9ae878479c05f66
YAN Wenkun <code@yanwk.fun>
2025-08-14 06:30:10 +0800

    'xpu' Add nodes

--- xpu/builder-scripts/generate-pak5.sh
+++ xpu/builder-scripts/generate-pak5.sh
@@ -6,19 +6,31 @@ echo '#' > pak5.txt
 array=(
 https://github.com/comfyanonymous/ComfyUI/raw/refs/heads/master/requirements.txt
 https://github.com/Comfy-Org/ComfyUI-Manager/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/ComfyUI_essentials/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/ComfyUI_FaceAnalysis/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/ComfyUI_InstantID/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/PuLID_ComfyUI/raw/refs/heads/main/requirements.txt
-https://github.com/Fannovel16/comfyui_controlnet_aux/raw/refs/heads/main/requirements.txt
-https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
+# Performance
+https://github.com/openvino-dev-samples/comfyui_openvino/raw/refs/heads/main/requirements.txt
+https://github.com/welltop-cn/ComfyUI-TeaCache/raw/refs/heads/main/requirements.txt
+https://github.com/city96/ComfyUI-GGUF/raw/refs/heads/main/requirements.txt
+# Workspace
+https://github.com/crystian/ComfyUI-Crystools/raw/refs/heads/main/requirements.txt
+# General
+https://github.com/ltdrdata/was-node-suite-comfyui/raw/refs/heads/main/requirements.txt
+https://github.com/kijai/ComfyUI-KJNodes/raw/refs/heads/main/requirements.txt
+https://github.com/jags111/efficiency-nodes-comfyui/raw/refs/heads/main/requirements.txt
+https://github.com/yolain/ComfyUI-Easy-Use/raw/refs/heads/main/requirements.txt
+# Control
 https://github.com/ltdrdata/ComfyUI-Impact-Pack/raw/refs/heads/Main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Impact-Subpack/raw/refs/heads/main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Inspire-Pack/raw/refs/heads/main/requirements.txt
-https://github.com/WASasquatch/was-node-suite-comfyui/raw/refs/heads/main/requirements.txt
-https://github.com/openvino-dev-samples/comfyui_openvino/raw/refs/heads/main/requirements.txt
-https://github.com/city96/ComfyUI-GGUF/raw/refs/heads/main/requirements.txt
-https://github.com/welltop-cn/ComfyUI-TeaCache/raw/refs/heads/main/requirements.txt
+https://github.com/Fannovel16/comfyui_controlnet_aux/raw/refs/heads/main/requirements.txt
+https://github.com/Gourieff/ComfyUI-ReActor/raw/refs/heads/main/requirements.txt
+https://github.com/huchenlei/ComfyUI-layerdiffuse/raw/refs/heads/main/requirements.txt
+https://github.com/kijai/ComfyUI-Florence2/raw/refs/heads/main/requirements.txt
+# Video
+https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
+https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/raw/refs/heads/main/requirements-no-cupy.txt
+https://github.com/melMass/comfy_mtb/raw/refs/heads/main/requirements.txt
+# Pending Removal
+https://github.com/cubiq/ComfyUI_essentials/raw/refs/heads/main/requirements.txt
 )
 
 for line in "${array[@]}";
