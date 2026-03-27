c2fb98e1a437f9164eded033690cbd7ed3b19be4
YAN Wenkun <code@yanwk.fun>
2026-03-28 03:03:57 +0800

    {base-cu130-pt211-cache} Update Nodes & deps

--- base-cu130-pt211-cache/builder-scripts/generate-pak5.sh
+++ base-cu130-pt211-cache/builder-scripts/generate-pak5.sh
@@ -10,57 +10,58 @@ https://github.com/Comfy-Org/ComfyUI-Manager/raw/refs/heads/main/requirements.tx
 https://github.com/city96/ComfyUI-GGUF/raw/refs/heads/main/requirements.txt
 https://github.com/nunchaku-ai/ComfyUI-nunchaku/raw/refs/heads/main/requirements.txt
 # Workspace
-https://github.com/crystian/ComfyUI-Crystools/raw/refs/heads/main/requirements.txt
-https://github.com/willmiao/ComfyUI-Lora-Manager/raw/refs/heads/main/requirements.txt
 https://github.com/alexopus/ComfyUI-Image-Saver/raw/refs/heads/master/requirements.txt
+https://github.com/crystian/ComfyUI-Crystools/raw/refs/heads/main/requirements.txt
+https://github.com/pydn/ComfyUI-to-Python-Extension/raw/refs/heads/main/requirements.txt
+https://github.com/willmiao/ComfyUI-Lora-Manager/raw/refs/heads/main/requirements.txt
+https://github.com/Amorano/Jovi_Help/raw/refs/heads/main/requirements.txt
 # General
 https://github.com/ltdrdata/was-node-suite-comfyui/raw/refs/heads/main/requirements.txt
-https://github.com/kijai/ComfyUI-KJNodes/raw/refs/heads/main/requirements.txt
 https://github.com/jags111/efficiency-nodes-comfyui/raw/refs/heads/main/requirements.txt
+https://github.com/kijai/ComfyUI-KJNodes/raw/refs/heads/main/requirements.txt
+https://github.com/mirabarukaso/ComfyUI_Mira/raw/refs/heads/main/requirements.txt
 https://github.com/yolain/ComfyUI-Easy-Use/raw/refs/heads/main/requirements.txt
-https://github.com/lihaoyun6/ComfyUI-llama-cpp_vlm/raw/refs/heads/main/requirements.txt
 # Control
 https://github.com/ltdrdata/ComfyUI-Impact-Pack/raw/refs/heads/Main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Impact-Subpack/raw/refs/heads/main/requirements.txt
 https://github.com/ltdrdata/ComfyUI-Inspire-Pack/raw/refs/heads/main/requirements.txt
+https://github.com/ClownsharkBatwing/RES4LYF/raw/refs/heads/main/requirements.txt
 https://github.com/Fannovel16/comfyui_controlnet_aux/raw/refs/heads/main/requirements.txt
 https://github.com/Gourieff/ComfyUI-ReActor/raw/refs/heads/main/requirements.txt
 https://github.com/huchenlei/ComfyUI-layerdiffuse/raw/refs/heads/main/requirements.txt
-https://github.com/kijai/ComfyUI-Florence2/raw/refs/heads/main/requirements.txt
-https://github.com/Ltamann/ComfyUI-TBG-SAM3/raw/refs/heads/main/requirements.txt
-https://github.com/ClownsharkBatwing/RES4LYF/raw/refs/heads/main/requirements.txt
-https://github.com/KohakuBlueleaf/z-tipo-extension/raw/refs/heads/main/requirements.txt
-# Video
-https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
-https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/raw/refs/heads/main/requirements-no-cupy.txt
-https://github.com/melMass/comfy_mtb/raw/refs/heads/main/requirements.txt
-https://github.com/FizzleDorf/ComfyUI_FizzNodes/raw/refs/heads/main/requirements.txt
-https://github.com/numz/ComfyUI-SeedVR2_VideoUpscaler/raw/refs/heads/main/requirements.txt
-# More
-https://github.com/1038lab/ComfyUI-QwenVL/raw/refs/heads/main/requirements.txt
-# Pending Removal
-# cubiq is no longer developing ComfyUI custom nodes
-https://github.com/cubiq/ComfyUI_essentials/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/ComfyUI_FaceAnalysis/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/PuLID_ComfyUI/raw/refs/heads/main/requirements.txt
-https://github.com/cubiq/ComfyUI_InstantID/raw/refs/heads/main/requirements.txt
-# Pending Removal 2
-# Most of these deps are already included in deps above
-https://github.com/akatz-ai/ComfyUI-AKatz-Nodes/raw/refs/heads/main/requirements.txt
-https://github.com/akatz-ai/ComfyUI-DepthCrafter-Nodes/raw/refs/heads/main/requirements.txt
-https://github.com/chflame163/ComfyUI_LayerStyle/raw/refs/heads/main/repair_dependency_list.txt
-https://github.com/chflame163/ComfyUI_LayerStyle/raw/refs/heads/main/requirements.txt
-https://github.com/digitaljohn/comfyui-propost/raw/refs/heads/master/requirements.txt
 https://github.com/Jonseed/ComfyUI-Detail-Daemon/raw/refs/heads/main/requirements.txt
-https://github.com/kijai/ComfyUI-DepthAnythingV2/raw/refs/heads/main/requirements.txt
-https://github.com/neverbiasu/ComfyUI-SAM2/raw/refs/heads/main/requirements.txt
-https://github.com/pydn/ComfyUI-to-Python-Extension/raw/refs/heads/main/requirements.txt
+https://github.com/KohakuBlueleaf/z-tipo-extension/raw/refs/heads/main/requirements.txt
+https://github.com/Ltamann/ComfyUI-TBG-SAM3/raw/refs/heads/main/requirements.txt
 # LayerStyle
 # Note that some deps in LayerStyle_Advance are outdated (causing trouble), pick wisely
 https://github.com/chflame163/ComfyUI_LayerStyle/raw/refs/heads/main/requirements.txt
 https://github.com/chflame163/ComfyUI_LayerStyle/raw/refs/heads/main/repair_dependency_list.txt
 #https://github.com/chflame163/ComfyUI_LayerStyle_Advance/raw/refs/heads/main/requirements.txt
 #https://github.com/chflame163/ComfyUI_LayerStyle_Advance/raw/refs/heads/main/repair_dependency_list.txt
+# Video
+https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/raw/refs/heads/main/requirements-no-cupy.txt
+https://github.com/FizzleDorf/ComfyUI_FizzNodes/raw/refs/heads/main/requirements.txt
+https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
+https://github.com/melMass/comfy_mtb/raw/refs/heads/main/requirements.txt
+https://github.com/numz/ComfyUI-SeedVR2_VideoUpscaler/raw/refs/heads/main/requirements.txt
+# More
+https://github.com/1038lab/ComfyUI-QwenVL/raw/refs/heads/main/requirements.txt
+https://github.com/akatz-ai/ComfyUI-DepthCrafter-Nodes/raw/refs/heads/main/requirements.txt
+https://github.com/digitaljohn/comfyui-propost/raw/refs/heads/master/requirements.txt
+https://github.com/kijai/ComfyUI-DepthAnythingV2/raw/refs/heads/main/requirements.txt
+https://github.com/kijai/ComfyUI-Florence2/raw/refs/heads/main/requirements.txt
+https://github.com/lihaoyun6/ComfyUI-llama-cpp_vlm/raw/refs/heads/main/requirements.txt
+https://github.com/pythongosssss/ComfyUI-WD14-Tagger/raw/refs/heads/main/requirements.txt
+# Pending Removal
+# cubiq is no longer developing ComfyUI custom nodes
+https://github.com/cubiq/ComfyUI_essentials/raw/refs/heads/main/requirements.txt
+https://github.com/cubiq/ComfyUI_FaceAnalysis/raw/refs/heads/main/requirements.txt
+https://github.com/cubiq/ComfyUI_InstantID/raw/refs/heads/main/requirements.txt
+https://github.com/cubiq/PuLID_ComfyUI/raw/refs/heads/main/requirements.txt
+# Pending Removal 2
+# Not in use but keep for extended packages
+https://github.com/akatz-ai/ComfyUI-AKatz-Nodes/raw/refs/heads/main/requirements.txt
+https://github.com/neverbiasu/ComfyUI-SAM2/raw/refs/heads/main/requirements.txt
 )
 
 for line in "${array[@]}";
