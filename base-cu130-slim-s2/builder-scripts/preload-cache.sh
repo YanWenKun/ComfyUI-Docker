1b01e512fc16de52fbdeb37d9685c0e98bcd62fc
YAN Wenkun <code@yanwk.fun>
2026-03-29 16:38:17 +0800

    {base-cu130-slim-s2} No longer install git repo version of ComfyUI-Manager

--- base-cu130-slim-s2/builder-scripts/preload-cache.sh
+++ base-cu130-slim-s2/builder-scripts/preload-cache.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-gcs() {
-    git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules "$@"
-}
-
 echo "########################################"
-echo "[INFO] Downloading ComfyUI & Nodes..."
+echo "[INFO] Downloading ComfyUI..."
 echo "########################################"
 
 mkdir -p /default-comfyui-bundle
@@ -20,14 +16,11 @@ cd /default-comfyui-bundle/ComfyUI
 # Using stable version (has a release tag)
 git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
 
-cd /default-comfyui-bundle/ComfyUI/custom_nodes
-gcs https://github.com/Comfy-Org/ComfyUI-Manager.git
-
 echo "########################################"
 echo "[INFO] Downloading Models..."
 echo "########################################"
 
 cd /default-comfyui-bundle/ComfyUI/models/vae_approx
-gcs https://github.com/madebyollin/taesd.git
+git clone --depth=1 --no-tags https://github.com/madebyollin/taesd.git
 cp taesd/*.pth .
 rm -rf taesd
